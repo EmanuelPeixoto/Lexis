ffb889ccfcf57a8082f410ab83303a2240b0f849
EmanuelPeixoto <leunamepeixoto@gmail.com>
2024-08-22 17:26:24 -0300

    gohtml filetype added, flake updated

--- config/lsp.nix
+++ config/lsp.nix
@@ -3,7 +3,7 @@
     enable = true;
 
     servers = {
-      # bashls.enable = true;                 # Bash
+      bashls.enable = true;                 # Bash
       clangd.enable = true;                 # C
       gopls.enable = true;                  # Go
       java-language-server.enable = true;   # Java
@@ -15,6 +15,8 @@
       emmet-ls.enable = true;               # HTML/CSS/SCSS/JS
     };
   };
+
+  filetype.extension.gohtml = "html";
 
   plugins.cmp = {
     enable = true;
