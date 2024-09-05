e8d2d66a9ee17eff4e98a2186bf8842fb105f610
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-09-05 17:45:28 -0300

    added plugins and updated flake
    added:
    autopairs
    gitsigns
    indent-blankline
    todo
    trouble
    vimtex
    wilder

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -17,31 +17,4 @@
   };
 
   filetype.extension.gohtml = "html";
-
-  plugins.cmp = {
-    enable = true;
-    autoEnableSources = true;
-    settings = {
-      sources = [
-        {name = "buffer";}
-        {name = "clangd";}
-        {name = "cssls";}
-        {name = "emmet_ls";}
-        {name = "eslint";}
-        {name = "gopls";}
-        {name = "html";}
-        {name = "jsonls";}
-        {name = "luasnip";}
-        {name = "nixd";}
-        {name = "nvim_lsp";}
-        {name = "path";}
-        {name = "texlab";}
-      ];
-
-      mapping = {
-        "<CR>" = "cmp.mapping.confirm({ select = true })";
-        "<Tab>" = "cmp.mapping.select_next_item()";
-      };
-    };
-  };
 }
