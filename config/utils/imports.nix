8850a10db6fb2fa9ff235baf5c34503a99ca1f6a
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-10-18 20:53:02 -0300

    added falsh, added nohls, updated flake

--- config/utils/imports.nix
+++ config/utils/imports.nix
@@ -3,8 +3,10 @@
     ./auto-pairs.nix
     ./codesnap.nix
     ./diffview.nix
+    ./flash.nix
     ./gitsigns.nix
     ./indent-blankline.nix
+    ./nohlsearch.nix
     ./render-markdown.nix
     ./telescope.nix
     ./todo.nix
