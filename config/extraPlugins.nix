087da8207ee6b033f5fec588d42c2e218e3d3f5d
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-10-18 21:21:26 -0300

    enhance autocompletion with new sources

--- config/extraPlugins.nix
+++ config/extraPlugins.nix
@@ -3,5 +3,9 @@
   extraPlugins = with pkgs.vimPlugins; [
     vim-nix
     yuck-vim
+
+    cmp_luasnip
+    cmp-buffer
+    cmp-path
   ];
 }
--- config/lsp/cmp.nix
+++ config/lsp/cmp.nix
@@ -1,16 +1,25 @@
 {
-  plugins.cmp = {
-    enable = true;
-    autoEnableSources = true;
-    settings = {
-      sources = [
-        {name = "nvim_lsp";}
-      ];
+  plugins = {
+    luasnip.enable = true;
 
-      mapping = {
-        "<CR>" = "cmp.mapping.confirm({ select = true })";
-        "<S-Tab>" = "cmp.mapping.select_prev_item()";
-        "<Tab>" = "cmp.mapping.select_next_item()";
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings = {
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "luasnip"; }
+          { name = "buffer"; }
+          { name = "path"; }
+        ];
+
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-Tab>" = "cmp.mapping.select_prev_item()";
+          "<Tab>" = "cmp.mapping.select_next_item()";
+        };
+
+        snippet.expand = "luasnip";
       };
     };
   };
