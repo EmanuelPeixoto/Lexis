60babdaa0c6b63ae2d7a19a84ba2b1efc05844ce
EmanuelPeixoto <leunamepeixoto@gmail.com>
2025-10-18 21:19:03 -0300

    added gitsigns shortcuts

--- config/utils/gitsigns.nix
+++ config/utils/gitsigns.nix
@@ -13,4 +13,12 @@
       };
     };
   };
+
+  keymaps = [
+    { key = "]c"; mode = "n"; action = "gitsigns.next_hunk"; options.desc = "Next Hunk"; }
+    { key = "[c"; mode = "n"; action = "gitsigns.prev_hunk"; options.desc = "Prev Hunk"; }
+    { key = "<leader>hs"; mode = "n"; action = "gitsigns.stage_hunk"; options.desc = "Stage Hunk"; }
+    { key = "<leader>hr"; mode = "n"; action = "gitsigns.reset_hunk"; options.desc = "Reset Hunk"; }
+    { key = "<leader>hp"; mode = "n"; action = "gitsigns.preview_hunk"; options.desc = "Preview Hunk"; }
+  ];
 }
